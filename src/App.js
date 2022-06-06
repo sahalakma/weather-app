add2174f1d704f59cddc0312d6be1da80591e644
sahal akma <69509304+sahalakma@users.noreply.github.com>
2022-06-06 15:49:08 +0530

    Changed Background Image

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <>  
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp >16) ? 'app warm': "app"): "app"}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp >16) ? 'app warm': "app cool"): "app"}>
        <main>
         <div className="search-box">
          <input 
